b625e35dfa7e7a0621d442cea79fdba3547982b6
mykhailo-sherhin <mykhailo.sherhin@gmail.com>
2022-05-26 11:20:25 +0300

    initial commit

--- Test Project/test.py
+++ Test Project/test.py
@@ -1,2 +1,8 @@
-import numpy as np
-print('str')
+import pandas as pd
+
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+print(countries)
